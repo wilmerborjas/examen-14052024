37f9eac99172539feb8d3c3a99494f5562128bfb
wilmer Borjas <wilmer.aprendiendo.programacion@gmail.com>
2024-05-29 10:42:54 +0200

    agrego cambios en la clase factura y los imprimo en consola

--- src/main/java/com/wbg/app/ExamenApp.java
+++ src/main/java/com/wbg/app/ExamenApp.java
@@ -1,8 +1,22 @@
 package com.wbg.app;
 
+import com.wbg.app.dto.Persona;
+import com.wbg.app.dto.Producto;
+import com.wbg.app.servicio.Factura;
+
 public class ExamenApp {
 	public static void main(String[] args) {
+		Producto producto = new Producto();
+		producto.setCantidadProducto(4);
+		producto.setValorVenta(10.0);
+
+		Persona persona = new Persona();
+		persona.setNombre("Carlos");
+
+		Factura factura = new Factura();
+		factura.getFactura(persona, producto);
 		
+	 
 	}
 
 }
--- src/main/java/com/wbg/app/servicio/Factura.java
+++ src/main/java/com/wbg/app/servicio/Factura.java
@@ -5,8 +5,14 @@ import com.wbg.app.dto.Producto;
 
 public class Factura {
 
-	public void getFactura(Persona persona, Producto producto) {
+	public void  getFactura(Persona persona, Producto producto) {
+		System.out.println("cantidad deproducto = " + producto.getCantidadProducto());
+		System.out.println("Valor venta = " + producto.getValorVenta());
+		System.out.println("Nombre = " + persona.getNombre());
 
+		double valorPagar = producto.getValorVenta() * producto.getCantidadProducto();
+		System.out.println("valor pagar  = " + valorPagar);
+		
 	}
 
 }
